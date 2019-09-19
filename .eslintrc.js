8e2755f6d78697dbdd145661c6f485dba044e49d
Kamaal Farah <kamaal.f1@gmail.com>
2019-09-19 14:18:37 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'max-len': [
+      'error',
+      {
+        code: 100,
+      },
+    ],
+    'react/jsx-filename-extension': ['off'],
+  },
+};
---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text style={{ textAlignVertical: 'center', textAlign: 'center' }}>
+      Hello
+    </Text>
+  </View>
+);
