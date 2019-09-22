4b7d10fc632168431f792cd07de07af800802e7f
Kamaal Farah <kamaal.f1@gmail.com>
2019-09-22 15:29:00 +0200

    Created a home screen and styled launchlist component

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,14 +1,16 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+import Home from './Views/Home';
 import LaunchList from './components/LaunchList';
 
 const Views = {
+  HomeScreen: { screen: Home },
   LaunchListScreen: { screen: LaunchList },
 };
 
 export default createAppContainer(
   createStackNavigator(Views, {
-    initialRouteName: 'LaunchListScreen',
+    initialRouteName: 'HomeScreen',
   }),
 );
---
+++ src/Views/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+import { screenHeight, screenWidth } from '../dimensions';
+
+export default StyleSheet.create({
+  /**  Home Styles 🎨 */
+  navigationContainer: {},
+  /** End Home Styles 🎨 */
+});
